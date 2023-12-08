f09a1113a732f248ec009688dfcf1429cea4f4f7
jokerwrld999 <ifalaleev49@gmail.com>
2023-12-08 12:45:53 +0200

    [terraform backend] Created Tf Cloud backend

--- main.tf
+++ main.tf
@@ -1,1 +1,22 @@
+terraform {
+  cloud {
+    organization = "jokerwrld"
 
+    workspaces {
+      name = "terraform-bootcamp"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.5.0"
+}
+
+provider "aws" {
+  region = var.region
+}
