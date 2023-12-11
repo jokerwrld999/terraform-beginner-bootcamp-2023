8eda0b2db5063ed6ecfea1807b7e3f276b96c0ec
jokerwrld999 <ifalaleev49@gmail.com>
2023-12-11 19:34:59 +0200

    [cloudfront] CloudFront Distribution created

--- main.tf
+++ main.tf
@@ -51,54 +51,6 @@ resource "aws_s3_bucket_versioning" "bootcamp_bucket_versioning" {
   }
 }
 
-# S3 bucket ACL access
-
-resource "aws_s3_bucket_ownership_controls" "bootcamp_bucket_ownership" {
-  bucket = aws_s3_bucket.bootcamp_bucket.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "bootcamp_bucket_access_block" {
-  bucket = aws_s3_bucket.bootcamp_bucket.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_acl" "bootcamp_bucket_acl" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.bootcamp_bucket_ownership,
-    aws_s3_bucket_public_access_block.bootcamp_bucket_access_block,
-  ]
-
-  bucket = aws_s3_bucket.bootcamp_bucket.id
-  acl    = "public-read"
-}
-
-# S3 bucket policy
-
-resource "aws_s3_bucket_policy" "bootcamp_bucket_policy" {
-  bucket = aws_s3_bucket.bootcamp_bucket.id
-
-  policy = <<POLICY
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "PublicReadGetObject",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": "s3:GetObject",
-            "Resource": "arn:aws:s3:::${aws_s3_bucket.bootcamp_bucket.bucket}/*"
-        }
-    ]
-}
-POLICY
-}
 
 # Upload website content
 
@@ -119,6 +71,106 @@ resource "aws_s3_object" "provision_source_files" {
   content = each.value.content
 }
 
+# CloudFront
+resource "aws_cloudfront_origin_access_control" "cloudfront_s3_oac" {
+  name                              = "CloudFront S3 OAC"
+  description                       = "Cloud Front S3 OAC"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "${aws_s3_bucket.bootcamp_bucket.id}"
+}
+
+resource "aws_cloudfront_distribution" "cloudfront_distribution" {
+
+  origin {
+    domain_name = aws_s3_bucket.bootcamp_bucket.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.bootcamp_bucket.id
+
+    origin_access_control_id = aws_cloudfront_origin_access_control.cloudfront_s3_oac.id
+  }
+
+  enabled = true
+  default_root_object = "index.html"
+    default_cache_behavior {
+      allowed_methods  = ["GET", "HEAD"]
+      cached_methods   = ["GET", "HEAD"]
+      target_origin_id = aws_s3_bucket.bootcamp_bucket.id
+
+      forwarded_values {
+        query_string = false
+
+        cookies {
+          forward = "none"
+        }
+      }
+
+      viewer_protocol_policy = "allow-all"
+      min_ttl                = 0
+      default_ttl            = 3600
+      max_ttl                = 86400
+    }
+
+    viewer_certificate {
+      cloudfront_default_certificate = true
+    }
+
+    restrictions {
+      geo_restriction {
+        restriction_type = "none"
+        locations        = []
+      }
+    }
+
+}
+
+# S3 bucket ACL access
+
+resource "aws_s3_bucket_ownership_controls" "bootcamp_bucket_ownership" {
+  bucket = aws_s3_bucket.bootcamp_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "bootcamp_bucket_access_block" {
+  bucket = aws_s3_bucket.bootcamp_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+}
+
+# S3 bucket policy
+
+resource "aws_s3_bucket_policy" "bootcamp_bucket_policy" {
+  bucket = aws_s3_bucket.bootcamp_bucket.id
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AllowCloudFrontServicePrincipalReadOnly",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "cloudfront.amazonaws.com"
+            },
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::${aws_s3_bucket.bootcamp_bucket.bucket}/*",
+            "Condition": {
+                "StringEquals": {
+                    "AWS:SourceArn": "${aws_cloudfront_distribution.cloudfront_distribution.arn}"
+                }
+            }
+        }
+    ]
+}
+POLICY
+}
+
 output "website_url" {
-  value = "http://${aws_s3_bucket.bootcamp_bucket.bucket}.s3-website.${var.region}.amazonaws.com"
+  value = "https://${aws_cloudfront_distribution.cloudfront_distribution.domain_name}"
 }
