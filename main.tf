02895b1bdb5cc60079f5a646037ffd8fe3bc6fd1
jokerwrld999 <ifalaleev49@gmail.com>
2023-12-12 23:06:04 +0200

    [cloudfront] Small change

--- main.tf
+++ main.tf
@@ -72,6 +72,7 @@ resource "aws_s3_object" "provision_source_files" {
 }
 
 # CloudFront
+
 resource "aws_cloudfront_origin_access_control" "cloudfront_s3_oac" {
   name                              = "CloudFront S3 OAC"
   description                       = "Cloud Front S3 OAC"
@@ -95,36 +96,36 @@ resource "aws_cloudfront_distribution" "cloudfront_distribution" {
 
   enabled = true
   default_root_object = "index.html"
-    default_cache_behavior {
-      allowed_methods  = ["GET", "HEAD"]
-      cached_methods   = ["GET", "HEAD"]
-      target_origin_id = aws_s3_bucket.bootcamp_bucket.id
 
-      forwarded_values {
-        query_string = false
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.bootcamp_bucket.id
 
-        cookies {
-          forward = "none"
-        }
-      }
+    forwarded_values {
+      query_string = false
 
-      viewer_protocol_policy = "allow-all"
-      min_ttl                = 0
-      default_ttl            = 3600
-      max_ttl                = 86400
-    }
-
-    viewer_certificate {
-      cloudfront_default_certificate = true
-    }
-
-    restrictions {
-      geo_restriction {
-        restriction_type = "none"
-        locations        = []
+      cookies {
+        forward = "none"
       }
     }
 
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
 }
 
 # S3 bucket ACL access
