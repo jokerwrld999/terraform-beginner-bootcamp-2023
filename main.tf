0b2eba7f74be5d9a9a70de45e1cd8a4619b00a08
JokerWrld <47574931+jokerwrld999@users.noreply.github.com>
2023-12-21 16:28:24 +0200

    5 custom cloudfront domain (#6)
    * Custom DNS from Cloudflare
    
    * Custom DNS from Cloudflare
    
    * Custom DNS from Cloudflare

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 # S3 static website bucket
-
 resource "aws_s3_bucket" "bootcamp_bucket" {
-  bucket = "terraform-bootcamp-jokerwrld"
+  bucket = "${var.root_domain_name}"
 }
 
 resource "aws_s3_bucket_website_configuration" "bootcamp_bucket_website" {
@@ -25,7 +24,6 @@ resource "aws_s3_bucket_versioning" "bootcamp_bucket_versioning" {
 }
 
 # Upload website content
-
 module "template_files" {
   source = "hashicorp/dir/template"
 
@@ -43,8 +41,30 @@ resource "aws_s3_object" "provision_source_files" {
   content = each.value.content
 }
 
-# CloudFront
+# Use the AWS Certificate Manager to create an SSL cert for our domain.
+resource "aws_acm_certificate" "certificate" {
+  domain_name       = "*.${var.root_domain_name}"
+  validation_method = "DNS"
 
+  subject_alternative_names = ["${var.root_domain_name}"]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Create Validation Record on Cloudflare
+resource "cloudflare_record" "cloudflare_validation_record" {
+  zone_id = var.cloudflare_zone_id
+  name    = "${tolist(aws_acm_certificate.certificate.domain_validation_options)[0].resource_record_name}"
+  value   = "${tolist(aws_acm_certificate.certificate.domain_validation_options)[0].resource_record_value}"
+  type    = "CNAME"
+  proxied = false
+
+  depends_on = [ aws_acm_certificate.certificate ]
+}
+
+# CloudFront
 resource "aws_cloudfront_origin_access_control" "cloudfront_s3_oac" {
   name                              = "CloudFront S3 OAC"
   description                       = "Cloud Front S3 OAC"
@@ -58,21 +78,21 @@ resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
 }
 
 resource "aws_cloudfront_distribution" "cloudfront_distribution" {
-
   origin {
     domain_name = aws_s3_bucket.bootcamp_bucket.bucket_regional_domain_name
-    origin_id   = aws_s3_bucket.bootcamp_bucket.id
+    origin_id   = "${var.sub_domain_name}"
 
     origin_access_control_id = aws_cloudfront_origin_access_control.cloudfront_s3_oac.id
   }
 
+  aliases = ["${var.sub_domain_name}"]
   enabled = true
   default_root_object = "index.html"
 
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = aws_s3_bucket.bootcamp_bucket.id
+    target_origin_id = "${var.sub_domain_name}"
 
     forwarded_values {
       query_string = false
@@ -89,7 +109,8 @@ resource "aws_cloudfront_distribution" "cloudfront_distribution" {
   }
 
   viewer_certificate {
-    cloudfront_default_certificate = true
+    acm_certificate_arn = "${aws_acm_certificate.certificate.arn}"
+    ssl_support_method = "sni-only"
   }
 
   restrictions {
@@ -101,7 +122,6 @@ resource "aws_cloudfront_distribution" "cloudfront_distribution" {
 }
 
 # S3 bucket ACL access
-
 resource "aws_s3_bucket_ownership_controls" "bootcamp_bucket_ownership" {
   bucket = aws_s3_bucket.bootcamp_bucket.id
   rule {
@@ -117,7 +137,6 @@ resource "aws_s3_bucket_public_access_block" "bootcamp_bucket_access_block" {
 }
 
 # S3 bucket policy
-
 resource "aws_s3_bucket_policy" "bootcamp_bucket_policy" {
   bucket = aws_s3_bucket.bootcamp_bucket.id
 
@@ -142,4 +161,12 @@ resource "aws_s3_bucket_policy" "bootcamp_bucket_policy" {
     ]
 }
 POLICY
+}
+
+resource "cloudflare_record" "cloudflare_record" {
+  zone_id = var.cloudflare_zone_id
+  name    = "barista"
+  value   = aws_cloudfront_distribution.cloudfront_distribution.domain_name
+  type    = "CNAME"
+  proxied = true
 }
